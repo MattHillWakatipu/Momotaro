ed8dde9001af1eca9a2a8bb149aa5cc78677817c
Matt <50199412+MattHillWakatipu@users.noreply.github.com>
2021-08-09 22:18:09 +1200

    Create blank Unreal project

---
+++ Source/Momotaro/MomotaroGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MomotaroGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOMOTARO_API AMomotaroGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
